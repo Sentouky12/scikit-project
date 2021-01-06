efa710afe415c5e8ad595c0cab7ed298cd3c31d6
Sentouky12 <74101180+Sentouky12@users.noreply.github.com>
2021-01-06 19:52:32 +0200

    Classifiers

--- mylibrary/setup.py
+++ mylibrary/setup.py
@@ -9,4 +9,14 @@ setup(name='mylibrary',
       description='Test',
       author='Shahnawaz Ahmed, Nathan Shammah',
       packages = ['mylibrary']
+     
+ classifiers=[
+        "License :: OSI Approved :: BSD License,
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
+       "Topic :: Documentation",
+       "Topic :: Scientific/Engineering",
+       "Intended Audience :: Developers",
+       "Intended Audience :: Science/Research",
+    ],
      )
